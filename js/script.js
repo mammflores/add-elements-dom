49119f6e55f799d2779be380ecb2d8e452e36c12
Marina <marinaflomar@gmail.com>
2024-06-14 16:18:44 +0200

    blabla

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
  // Aquí tu código
+
+
+ console.log("hola mundo")
+ const boton = document.getElementById('agregar')
+  const lista = document.getElementById('lista')
+  const buttonreset = document.getElementById('reset')
+  
+  
+  
+  boton.addEventListener('click', function(){
+     const producto = prompt('¿cuáles conoces?');
+     if(producto){
+         let nuevoItem = document.createElement('li');
+         nuevoItem.textContent = producto;
+         lista.appendChild(nuevoItem);
+     }
+ 
+  })
+ 
+  buttonreset.addEventListener('click', function(){
+   lista.innerHTML = '';
+  });
